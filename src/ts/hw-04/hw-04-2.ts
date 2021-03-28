6665dc20b427d201f9a943dff8bc06c641435977
Ganenco Dmitri <ganenko.dmitri@gmail.com>
2021-03-28 19:18:38 +0300

    add hw-04

--- src/ts/hw-04/hw-04-2.ts
+++ src/ts/hw-04/hw-04-2.ts
@@ -1,61 +1,54 @@
-/**
- * Задача 4-2
-Callback функция и метод push
+// /**
+//  * Задача 4-2
+// Callback функция и метод push
 
-Функция isUniq принимает три параметра - element, index и arr. 
-Функция возвращает true или false в зависимости от того
- встречается ли элемент первый раз в массиве (true)
-  или этот элемент в массиве уже встречался (false) .
+// Функция isUniq принимает три параметра - element, index и arr.
+// Функция возвращает true или false в зависимости от того
+//  встречается ли элемент первый раз в массиве (true)
+//   или этот элемент в массиве уже встречался (false) .
 
-Функция isEven принимает один параметр - element. 
-Функция возвращает true или false в зависимости от того
- является ли элемент четным числом или нет.
+// Функция isEven принимает один параметр - element.
+// Функция возвращает true или false в зависимости от того
+//  является ли элемент четным числом или нет.
 
-Функция filterArray(array, cb), принимает 1-м параметром array - массив чисел,
-а вторым параметром cb - функцию обратного вызова (callback). 
-Дополни тело функции так,
-чтобы функция filterArray заполняла новый пустой массив numbers
-только теми элементами из массива array, 
-для которых вызов функции cb вернет true. 
- */
+// Функция filterArray(array, cb), принимает 1-м параметром array - массив чисел,
+// а вторым параметром cb - функцию обратного вызова (callback).
+// Дополни тело функции так,
+// чтобы функция filterArray заполняла новый пустой массив numbers
+// только теми элементами из массива array,
+// для которых вызов функции cb вернет true.
+//  */
 
-const isUniq = (
-  element: number,
-  index: number,
-  array?: string[] | number[] | any,
-) => array.indexOf(element) === index;
-const isEven = (element: number) => element % 2 === 0;
+// const isUniq = (element: number, index: number, array: number[]) =>
+//   array.indexOf(element) === index;
+// const isEven = (element: number) => element % 2 === 0;
 
-function filterArray(
-  array: number[],
-  cb: (
-    element: number,
-    index: number,
-    array?: string[] | number[] | any,
-  ) => number[],
-) {
-  //   'use strict';
-  const numbers = [];
-  for (let i = 0; i < array.length; i += 1) {
-    const element = array[i];
-    const index = i;
-    // console.log(index);
-    // console.log(element);
-    console.log(cb);
-    // Write code under this line
-    if (cb(element, index, array)) {
-      numbers.push(element);
-    }
-  }
-  return numbers;
-}
+// function filterArray(
+//   array: number[],
+//   cb: (element?: number, index?: number, array?: number[]) => number[] | void,
+// ) {
+//   //   'use strict';
+//   const numbers = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     const element = array[i];
+//     const index = i;
+//     // console.log(index);
+//     // console.log(element);
+//     console.log(cb);
+//     // Write code under this line
+//     if (cb(element, index, array)) {
+//       numbers.push(element);
+//     }
+//   }
+//   return numbers;
+// }
 
-const arr = [1, 2, 3, 4, 5, 1, 2, 5];
+// const arr = [1, 2, 3, 4, 5, 1, 2, 5];
 
-console.log(filterArray(arr, isUniq));
+// console.log(filterArray(arr, isUniq));
 // [1, 2, 3, 4, 5]
 
-console.log(filterArray(arr, isEven));
+// console.log(filterArray(arr, isEven));
 
 // [2, 4, 2]
 export {};
---
+++ src/ts/hw-04/hw-04-4.ts
@@ -0,0 +1,50 @@
+/**
+ * Задача 4-4
+this в методах объекта
+Расставь отсутствующие this в методах объекта account.
+
+В комментариях показаны операции с объектом и ожидаемые результаты.
+ */
+interface PlaginUccount {
+  owner: string;
+  balance: number;
+  discount: number;
+  orders: string[];
+  changeDiscount: (value: number) => void;
+  showOrders: () => string[];
+  addOrder: (cost: number, order: string) => void;
+}
+
+const account: PlaginUccount = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value: number) {
+    this.discount = value; // Write code in this line
+  },
+  showOrders() {
+    return this.orders; // Write code in this line
+  },
+  addOrder(cost: number, order: string) {
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
+  },
+};
+const copyAccount = Object.assign({}, account);
+copyAccount.orders = [...account.orders];
+// копируем для автотестов ссылочные типы
+
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3']
+
+// account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3', 'order-4']
+
+export {};
